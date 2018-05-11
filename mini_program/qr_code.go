de9079c43e1e7ca08005670470825156febac818
Zoelov <wanghao000.cool@163.com>
2018-05-11 14:36:42 +0800

    qr code

---
+++ mini_program/qr_code.go
@@ -0,0 +1,37 @@
+package mini_program
+
+import (
+	"fmt"
+	"miniprogram/utils"
+)
+
+type Color struct {
+	Red   int `json:"r"`
+	Green int `json:"g"`
+	Blue  int `json:"b"`
+}
+
+type MiniProgramQRCodeParam struct {
+	Scene     string `json:"scene"`
+	Page      string `json:"page"`
+	Width     int    `json:"width"`
+	AutoColor bool   `json:"auto_color"`
+	LineColor Color  `json:"line_color"`
+}
+
+// GetMiniProgramQRCode -- 获取永久小程序码
+func GetMiniProgramQRCode(param *MiniProgramQRCodeParam, config MiniProgramConfig) ([]byte, error) {
+	token, _, err := GetMiniProgramAccessToken(config)
+	if err != nil {
+		return nil, err
+	}
+
+	url := fmt.Sprintf(qrCodeURL, token)
+	var response []byte
+	response, err = utils.PostJSON(url, param)
+	if err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
--- utils/http.go
+++ utils/http.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -16,6 +18,27 @@ func HTTPGet(uri string) ([]byte, error) {
 	defer response.Body.Close()
 	if response.StatusCode != http.StatusOK {
 		return nil, fmt.Errorf("http get error: uri=%v, statusCode=%v", uri, response.StatusCode)
+	}
+
+	return ioutil.ReadAll(response.Body)
+}
+
+func PostJSON(uri string, obj interface{}) ([]byte, error) {
+	jsonData, err := json.Marshal(obj)
+	if err != nil {
+		return nil, err
+	}
+
+	body := bytes.NewBuffer(jsonData)
+	response, err := http.Post(uri, "application/json;charset=utf-8", body)
+	if err != nil {
+		return nil, err
+	}
+
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("http post error: uri=%v, statusCode=%v", uri, response.StatusCode)
 	}
 
 	return ioutil.ReadAll(response.Body)
